6bab18e2ed0390e5699fb9ced4c64999198b6bf4
erarash <erani.arash@gmail.com>
2018-09-28 16:10:07 -0700

    working fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,62 +1,56 @@
 let app = {
-  init: function () {
+  init: function() {
     window.sanitizer = jSanity.sanitize;
-    $('.submit').on('submit', this.handleSubmit);
+    $(".submit").on("submit", this.handleSubmit);
   },
-  send: function (message) {
+  send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
-      type: 'POST',
+      url: "http://parse.la.hackreactor.com/chatterbox/classes/messages",
+      type: "POST",
       data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
+      contentType: "application/json",
+      success: function(data) {
+        console.log("chatterbox: Message sent");
       },
-      error: function (data) {
+      error: function(data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
+        console.error("chatterbox: Failed to send message", data);
       }
     });
   },
-  fetch: function () {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: undefined,
-      type: 'GET',
-      // data: JSON.stringify(),
-      // contentType: 'application/json',
-      success: function () {
-        console.log('chatterbox: Message sent');
-      },
-      error: function () {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
-      }
+  fetch: function() {
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: "http://parse.la.hackreactor.com/chatterbox/classes/messages",
+      method: "GET",
+    };
+
+    $.ajax(settings).done(function(response) {
+      console.log(response);
     });
   },
   clearMessages: function() {
-    $('#chats').html('');
+    $("#chats").html("");
   },
   renderMessage: function(message) {
-    var {username, text, roomname} = message;
+    var { username, text, roomname } = message;
     var messageNode = $.parseHTML(`<div id ="message"> </div>`);
-    var usernameNode = $.parseHTML(`<a href="" class="username"> ${_.escape(username)} </a>`)
+    var usernameNode = $.parseHTML(
+      `<a href="" class="username"> ${_.escape(username)} </a>`
+    );
     var textNode = $.parseHTML(`<p> ${_.escape(text)} </p>`);
-    $(usernameNode).click(this.handleUsernameClick)
+    $(usernameNode).click(this.handleUsernameClick);
     $(messageNode).append(usernameNode);
     $(messageNode).append(textNode);
-    
 
-    $('#chats').prepend(messageNode);
-    
-},
-  renderRoom: function(roomName) {
-    $('#roomSelect').prepend(`<blink> ${roomName} </blink>`)
+    $("#chats").prepend(messageNode);
   },
-  handleUsernameClick: function(){},
+  renderRoom: function(roomName) {
+    $("#roomSelect").prepend(`<blink> ${roomName} </blink>`);
+  },
+  handleUsernameClick: function() {},
 
-  handleSubmit: function(){
-  
-  }
+  handleSubmit: function() {}
 };
