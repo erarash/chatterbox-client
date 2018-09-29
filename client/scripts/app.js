a11a775a702974308596fb2580a15ef4ae1eef21
erarash <erani.arash@gmail.com>
2018-09-28 19:56:26 -0700

    feels50/50man

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,41 +1,48 @@
 let app = {
   init: function() {
+
     window.sanitizer = jSanity.sanitize;
-    $(".submit").on("submit", this.handleSubmit);
+    $("#send").submit(false);
+    $("#send").on("submit",function(event){
+      event.preventDefault();
+      this.handleSubmit();
+    })
+    //$(".submit").on("submit", this.handleSubmit);
     this.fetch();
   },
   send: function(message) {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
+    var settings = {
+      async: true,
       url: "http://parse.la.hackreactor.com/chatterbox/classes/messages",
-      type: "POST",
-      data: JSON.stringify(message),
-      contentType: "application/json",
-      success: function(data) {
-        console.log("chatterbox: Message sent");
-      },
-      error: function(data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error("chatterbox: Failed to send message", data);
-      }
-    });
+      method: "POST",
+      crossDomain: true,
+      processData: false,
+      contentType: 'application/json',
+      data: JSON.stringify(message)
+    };
+
+    $.ajax(settings).done(function(response) {
+      console.log(response);
+      this.handleSubmit();
+
+    }.bind(this));
   },
   fetch: function() {
-    
     var settings = {
       async: true,
       crossDomain: true,
       url: "http://parse.la.hackreactor.com/chatterbox/classes/messages",
       method: "GET",
-      
+      data: { order: "-createdAt" }
     };
 
-    $.ajax(settings).done(function(response) {
-        this.results = response.results
+    $.ajax(settings).done(
+      function(response) {
+        this.results = response.results;
         this.findAllActiveRooms();
         this.renderRoom();
-    }.bind(this));
-
+      }.bind(this)
+    );
   },
   clearMessages: function() {
     $("#chats").html("");
@@ -51,38 +58,39 @@ let app = {
     $(messageNode).append(usernameNode);
     $(messageNode).append(textNode);
 
-    $("#chats").prepend(messageNode);
+    $("#chats").append(messageNode);
   },
 
   findAllActiveRooms: function() {
-   this.activeRooms = [];
-   for (var i = 0; i < this.results.length; i++) {
+    this.activeRooms = [];
+    for (var i = 0; i < this.results.length; i++) {
       if (!this.activeRooms.includes(this.results[i].roomname)) {
         this.activeRooms.push(this.results[i].roomname);
-      } 
-   }
+      }
+    }
   },
 
   renderRoom: function(roomName) {
-    
     for (let i = 0; i < this.activeRooms.length; i++) {
-      $("#roomSelect").prepend(`<option value="${this.activeRooms[i]}">${this.activeRooms[i]}</option>`);
-    };
+      $("#roomSelect").append(
+        `<option value="${this.activeRooms[i]}">${this.activeRooms[i]}</option>`
+      );
+    }
     // TODO make it so that if you have more than one room
     // you can actually switch between them.
-    for (let i = 0; i < this.results.length; i++){
-    if (this.results[i].roomname){
-        this.renderMessage(this.results[i])
+    for (let i = 0; i < this.results.length; i++) {
+      if (this.results[i].roomname) {
+        this.renderMessage(this.results[i]);
+      }
     }
-  }
-  
   },
-
-
 
   handleUsernameClick: function() {},
 
-  handleSubmit: function() {}
+  handleSubmit: function() {
+
+    console.log('ayoooooo')
+  }
 };
 
-app.init()
+app.init();
